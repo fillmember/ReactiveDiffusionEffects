9b26de5b6c9b33a2fd34dc0922750ca3b600231f
FillMember <fillmember@gmail.com>
2016-08-21 12:29:42 +0200

    Add ReactiveDiffusionDisplacement codes

---
+++ Assets/FillMember/ReactiveDiffusionDisplacement.cs
@@ -0,0 +1,183 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace FillMember {
+
+	[RequireComponent(typeof(Camera))]
+	public class ReactiveDiffusionDisplacement : MonoBehaviour {
+
+		#region Public properties and methods
+
+		[Range(1,32)]
+		public uint iterations = 2;
+
+		[Range(0,1f)]
+		public float diffuseA = 0.85f;
+		[Range(0,1f)]
+		public float diffuseB = 1.00f;
+
+
+		[Range(0,0.2f)]
+		public float feedRate = 0.03500f;
+		[Range(0,0.2f)]
+		public float killRate = 0.06400f;
+
+		[Range(0.0001f,0.005f)]
+		public float texelSize = 0.0015f;
+
+		[Range(0,1)]
+		public float decayRate = 0.98f;
+
+		public bool distortOnly = false;
+
+		[Range(0,2)]
+		public int state = 0;
+
+		// public bool mosh = true;
+		// public bool distort = true;
+
+		#endregion
+
+
+		#region Private properties
+
+		Shader shader;
+
+		Material material;
+
+		RenderTexture rdBuffer = null;
+		RenderTexture workBuffer = null;
+		RenderTexture accumulated = null;
+
+		int lastFrame;
+
+		void ReleaseBuffer(RenderTexture buffer) {
+			if (buffer != null) RenderTexture.ReleaseTemporary(buffer);
+		}
+
+		RenderTexture NewBuffer(RenderTexture source) {
+			return RenderTexture.GetTemporary(source.width, source.height);
+		}
+
+		#endregion
+
+
+		#region Monobehaviour functions
+
+		void OnEnable() {
+
+			// Material
+			material = new Material (Shader.Find ("Hidden/FillMember/ReactiveDiffusionDisplacement"));
+			material.hideFlags = HideFlags.DontSave;
+
+			// Camera
+			GetComponent<Camera> ().depthTextureMode |=
+				DepthTextureMode.Depth | DepthTextureMode.MotionVectors;
+
+			state = 0;
+
+		}
+
+		void OnDisable() {
+
+			// Material
+			DestroyImmediate (material);
+			material = null;
+
+			// Buffers
+			ReleaseBuffer (rdBuffer);
+			ReleaseBuffer (workBuffer);
+			ReleaseBuffer (accumulated);
+			rdBuffer = null;
+			workBuffer = null;
+			accumulated = null;
+
+		}
+
+		void OnRenderImage( RenderTexture source , RenderTexture destination ) {
+
+			if (state == 0) {
+
+				// update buffers
+				ReleaseBuffer (rdBuffer);
+				rdBuffer = NewBuffer (source);
+				Graphics.Blit (source, rdBuffer);
+
+				ReleaseBuffer (workBuffer);
+				workBuffer = NewBuffer (source);
+				Graphics.Blit (source, workBuffer);
+
+				ReleaseBuffer( accumulated );
+				accumulated = NewBuffer( source );
+				Graphics.Blit (source, accumulated, material, 0);
+
+				// Render : copy source to destination
+				Graphics.Blit (source, destination);
+
+				material.SetTexture ("originalTex", workBuffer);
+
+			} else if (state == 1) {
+
+				if (Time.frameCount != lastFrame) {
+
+					material.SetFloat ("killRate", killRate);
+					material.SetFloat ("feedRate", feedRate);
+					material.SetFloat ("diffuseA", diffuseA);
+					material.SetFloat ("diffuseB", diffuseB);
+					material.SetFloat ("texelSize", texelSize);
+
+					material.SetTexture ("rdTex", rdBuffer);
+
+					for (int i = 0; i < iterations; i++) {
+						Graphics.Blit (rdBuffer, rdBuffer, material, 1);
+					}
+
+					lastFrame = Time.frameCount;
+
+					// calculate accumulated
+					material.SetTexture ("accumulatedMotionVector", accumulated);
+					material.SetFloat ("decayRate", decayRate);
+					Graphics.Blit (source, accumulated, material, 2);
+					
+				}
+
+				// write to destination
+				if ( distortOnly ) {
+					Graphics.Blit (source, workBuffer, material, 4);
+				} else {
+					Graphics.Blit (source, workBuffer, material, 3);
+				}
+
+				Graphics.Blit (workBuffer, destination);
+
+			} else if (state == 2) {
+
+				if (Time.frameCount != lastFrame) {
+
+					material.SetFloat ("killRate", killRate);
+					material.SetFloat ("feedRate", feedRate);
+					material.SetFloat ("diffuseA", diffuseA);
+					material.SetFloat ("diffuseB", diffuseB);
+					material.SetFloat ("texelSize", texelSize);
+
+					material.SetTexture ("rdTex", rdBuffer);
+
+					for (int i = 0; i < iterations; i++) {
+						Graphics.Blit (rdBuffer, rdBuffer, material, 1);
+					}
+
+					lastFrame = Time.frameCount;
+
+				}
+
+				Graphics.Blit (rdBuffer, destination);
+
+			}
+
+		}
+
+		#endregion
+
+	}
+
+}
---
+++ Assets/FillMember/Shader/ReactiveDiffusionDisplacement.shader
@@ -0,0 +1,158 @@
+﻿Shader "Hidden/FillMember/ReactiveDiffusionDisplacement"
+{
+	Properties
+	{
+
+		_MainTex("", 2D) = ""{}
+
+	}
+
+    CGINCLUDE
+
+    #include "UnityCG.cginc"
+
+    sampler2D _MainTex;
+    float4 _MainTex_TexelSize;
+
+    sampler2D rdTex;
+	float4 rdTex_TexelSize;
+
+    sampler2D originalTex;
+	float4 originalTex_TexelSize;
+
+	sampler2D accumulatedMotionVector;
+	float4 accumulatedMotionVector_TexelSize;
+
+	float texelSize;
+
+	float diffuseA;
+	float diffuseB;
+
+	float feedRate;
+	float killRate;
+
+	float decayRate;
+
+	// Motion Vector
+	sampler2D_half _CameraMotionVectorsTexture;
+	float4 _CameraMotionVectorsTexture_TexelSize;
+
+	fixed4 frag_rd(v2f_img source) : SV_Target {
+
+		float2 v0 = tex2D( rdTex , source.uv ).rg ;
+
+		half2 mv = tex2D( _CameraMotionVectorsTexture , source.uv ).rg;
+
+		float laplaceFactor = lerp( 0.0 , 1.1 , length(mv) );
+		float claplaceFactor = (1 - laplaceFactor) * 0.25;
+
+		float2 laplace =
+			+ laplaceFactor * tex2D( rdTex , source.uv + mv ).rg
+			+ claplaceFactor * (
+				tex2D( rdTex , source.uv + float2( -texelSize , 0.0 ) ).rg +
+				tex2D( rdTex , source.uv + float2( 0.0 , -texelSize ) ).rg +
+				tex2D( rdTex , source.uv + float2(  texelSize , 0.0 ) ).rg +
+				tex2D( rdTex , source.uv + float2( 0.0 ,  texelSize ) ).rg
+			)
+			- v0;
+
+		float reaction = v0.r * v0.g * v0.g;
+		float du = diffuseA * laplace.r - reaction + feedRate * ( 1.0 - v0.r );
+		float dv = diffuseB * laplace.g + reaction - ( feedRate + killRate ) * v0.g;
+
+		float2 dst = v0 + float2(du, dv) * 0.9;
+
+		dst.g += min(0.5,length(mv));
+
+		return float4( dst , mv.rg );
+
+	}
+
+	half4 frag_init(v2f_img source) : SV_Target {
+		return 0;
+	}
+
+	half4 frag_update(v2f_img source) : SV_Target {
+		half2 mv = tex2D( _CameraMotionVectorsTexture , source.uv ).rg;
+		half2 amv = tex2D( accumulatedMotionVector , source.uv ).rg;
+		return half4( (amv + mv) * decayRate , 0.0 , 1.0 );
+	}
+
+	half4 frag_disp(v2f_img source) : SV_Target {
+
+		float2 uv = source.uv;
+		float4 simulation = tex2D( rdTex , uv );
+		float4 acc = tex2D( accumulatedMotionVector , uv );
+
+		float2 newUv = uv + (simulation.ba + acc.rg) * simulation.g;
+
+		float4 main = tex2D( _MainTex , uv );
+		float4 original = tex2D( originalTex , uv );
+		float4 disp = tex2D( _MainTex , newUv );
+		float4 work = lerp( main , original , 0.8 + simulation.g );
+
+		return half4( lerp( work , disp , simulation.g ).rgb , 1.0 );
+
+	}
+
+	half4 frag_disp_distort(v2f_img source) : SV_Target {
+
+		float2 uv = source.uv;
+		float4 simulation = tex2D( rdTex , uv );
+		float4 acc = tex2D( accumulatedMotionVector , uv );
+		float4 disp = tex2D( _MainTex , uv + (simulation.ba + acc.rg) * simulation.g );
+
+		return half4( disp.rgb , 1.0 );
+
+	}
+
+	ENDCG
+
+	SubShader {
+
+		// No culling or depth
+		Cull Off ZWrite Off ZTest Less
+
+		Pass {
+			CGPROGRAM
+			#pragma vertex vert_img
+			#pragma fragment frag_init
+			#pragma target 3.0
+			ENDCG
+		}
+
+		Pass {
+			CGPROGRAM
+			#pragma vertex vert_img
+			#pragma fragment frag_rd
+			#pragma target 3.0
+			ENDCG
+		}
+
+		Pass {
+			CGPROGRAM
+			#pragma vertex vert_img
+			#pragma fragment frag_update
+			#pragma target 3.0
+			ENDCG
+		}
+
+		Pass {
+			CGPROGRAM
+			#pragma vertex vert_img
+			#pragma fragment frag_disp
+			#pragma target 3.0
+			ENDCG
+		}
+
+		Pass {
+			CGPROGRAM
+			#pragma vertex vert_img
+			#pragma fragment frag_disp_distort
+			#pragma target 3.0
+			ENDCG
+		}
+
+	}
+
+}
