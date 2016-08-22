6d8cfd0e54c5e1803f0e3726d18a738cb273dc4c
FillMember <fillmember@gmail.com>
2016-08-22 14:39:44 +0200

    Test Scene 001

---
+++ Assets/Test/Scripts/Rotator.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+	
+	public Vector3 rotation = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.Rotate ( rotation * Time.deltaTime );
+	
+	}
+}
---
+++ Assets/Test/Scripts/Wiggle.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wiggle : MonoBehaviour {
+
+	public Vector3 wigglePosition = Vector3.zero;
+	public Vector3 wiggleRotation = Vector3.zero;
+
+	public float frequency = 5.0f;
+
+	public bool aim = false;
+
+	public Transform aimTarget;
+
+	private Transform _trans;
+
+	// Use this for initialization
+	void Start () {
+
+		_trans = transform;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float sin = Mathf.Sin (Time.time * frequency) * Time.deltaTime;
+		Vector3 sinv = new Vector3 (sin, sin, sin);
+
+		_trans.Translate (Vector3.Scale (wigglePosition, sinv));
+		_trans.Rotate (Vector3.Scale (wiggleRotation, sinv));
+
+		if (aim) {
+
+			_trans.LookAt (aimTarget);
+
+		}
+	 
+	}
+}
